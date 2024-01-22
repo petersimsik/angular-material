b02ef5af1e88819264e785aa805260c46e284d75
Peter Simsik <peter.simsik@unilabs.com>
2024-01-22 16:38:34 +0100

    dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -11,7 +11,21 @@ import * as moment from 'moment';
 })
 export class CourseDialogComponent implements OnInit {
 
-    constructor(private fb: FormBuilder) {
+    description : string;
+
+    form = this.fb.group({
+       description: [this.course.description, Validators.required],
+       category: [this.course.category, Validators.required],
+       releasedAt: [new Date(), Validators.required],
+       longDescription: [this.course.longDescription, Validators.required]
+    });
+
+    constructor(
+        private fb: FormBuilder,
+        @Inject(MAT_DIALOG_DATA) private course: Course,
+        private dialogRef : MatDialogRef<CourseDialogComponent>
+        ) {
+    this.description = course.description;            
 
 
     }
@@ -20,5 +34,25 @@ export class CourseDialogComponent implements OnInit {
 
     }
 
+    close() {
+        this.dialogRef.close();
+    }
+
+    save() {
+        this.dialogRef.close(this.form.value);
+    }
+
 }
 
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+    const config = new MatDialogConfig();
+    config.disableClose = true;
+    config.autoFocus = true;
+
+    config.data = {
+        ...course
+    };
+
+    const dialogRef = dialog.open(CourseDialogComponent, config);
+    return dialogRef.afterClosed();
+}
